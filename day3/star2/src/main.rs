9dcb7f3f07979232f3922d9ab8ed73b58eababdf
rsahwe <rsahwe@gmx.net>
2025-12-04 02:08:03 +0100

    day3 made cooler

--- day3/star2/src/main.rs
+++ day3/star2/src/main.rs
@@ -1,38 +1,23 @@
-use std::{error::Error, fs};
-
-fn main() -> Result<(), Box<dyn Error>> {
-    let input = fs::read_to_string("../input1")?;
-
-    let mut sum = 0;
-
-    for line in input.lines() {
-        let line = line.trim();
-        if line.len() == 0 {
-            continue;
-        }
-
-        let chars = line.chars();
-        let count = chars.clone().count();
-
-        let mut skip = 0;
-        let mut number = 0;
-
-        for i in 0..12 {
-            let (index, chr) = chars
-                .clone()
-                .enumerate()
-                .skip(skip)
-                .take(count + i + 1 - skip - 12)
-                .fold((0, '0'), |(ai, ac), (ni, nc)| if nc as usize > ac as usize { (ni, nc) } else { (ai, ac) });
-
-            skip = index + 1;
-            number = number * 10 + chr as usize - '0' as usize;
-        }
-
-        sum += number;
-    }
-
-    println!("Sum: {sum}");
-
-    Ok(())
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    Ok(println!(
+        "Sum: {}",
+        std::fs::read_to_string("../input1")?
+            .lines()
+            .map(str::trim)
+            .filter(|line| line.len() != 0)
+            .map(|line| (line.bytes(), line.bytes().count()))
+            .map(|(chars, count)| (0..12)
+                .fold((0, 0), |(skip, number), i| chars
+                    .clone()
+                    .enumerate()
+                    .skip(skip)
+                    .take(count + i + 1 - skip - 12)
+                    .try_fold((0, b'0'), |(ai, ac), (ni, nc)| Some(
+                        (nc > ac).then_some((ni, nc)).unwrap_or((ai, ac))
+                    ))
+                    .map(|(index, chr)| (index + 1, number * 10 + (chr - b'0') as usize))
+                    .unwrap())
+                .1)
+            .sum::<usize>()
+    ))
 }
