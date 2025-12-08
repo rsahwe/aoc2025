00a7a1b03db17af56f391d810b45a65757f0e9ca
rsahwe <rsahwe@gmx.net>
2025-12-08 17:24:23 +0100

    day8

---
+++ day8/star2/src/main.rs
@@ -0,0 +1,68 @@
+use std::{error::Error, fs};
+
+use itertools::Itertools;
+use nalgebra::{Point3, distance};
+
+fn explore_region(visited: &mut Vec<bool>, p: usize, conn: &Vec<bool>) -> usize {
+    if visited[p] {
+        0
+    } else {
+        visited[p] = true;
+
+        1 + (0..visited.len()).filter_map(|i| conn[i * visited.len() + p].then(|| explore_region(visited, i, conn))).sum::<usize>()
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let input = fs::read_to_string("../input1")?;
+
+    let points = input
+        .lines()
+        .map(str::trim)
+        .filter(|line| line.len() > 0)
+        .map(|line| {
+            let (x, (y, z)) = line
+                .split_once(',')
+                .map(|(x, yz)| (x, yz.split_once(',').unwrap()))
+                .unwrap();
+
+            Point3::new(x.parse::<f64>().expect("Wrong number!!!"), y.parse().expect("Wrong number!!!"), z.parse().expect("Wrong number!!!"))
+        })
+        .collect::<Vec<_>>();
+
+    let mut connected = (0..points.len())
+        .cartesian_product(0..points.len())
+        .map(|(a, b)| a == b)
+        .collect::<Vec<_>>();
+
+    let mut product = 1.0;
+
+    while explore_region(&mut vec![false; points.len()], 0, &connected) != points.len() {
+        let (i, _) = connected
+            .iter()
+            .enumerate()
+            .filter(|(_, c)| !**c)
+            .map(|(i, _)| {
+                let x = i % points.len();
+                let y = i / points.len();
+
+                (i, distance(&points[x], &points[y]))
+            })
+            .sorted_by(|(_, ad), (_, bd)| f64::total_cmp(&ad, bd))
+            .next()
+            .expect("No connection left?");
+
+        let x = i % points.len();
+        let y = i / points.len();
+        connected[x * points.len() + y] = true;
+        connected[y * points.len() + x] = true;
+
+        product = points[x].x * points[y].x;
+    }
+
+
+
+    println!("Product: {product}");
+
+    Ok(())
+}
